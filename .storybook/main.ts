6df5ef79df78fee37698876ce9dc6c02ffce80c4
Kazuto <mail@kazuto.de>
2026-01-29 14:54:32 +0100

    chore(Storybook): Upgrade Storybook dependencies and update imports
    - Replace @storybook/addon-essentials with @storybook/addon-docs
    - Update import paths from @storybook/vue3 to @storybook/vue3-vite
    - Remove experimental addon test plugin from vitest config
    - Upgrade Storybook and related packages to v10.2.1
    - Update MDX Meta import path to use addon-docs/blocks
    - Switch storybook/test imports from @storybook/test namespace

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,9 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/vue3-vite",
