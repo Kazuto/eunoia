4e2d987e3816ae7e99e47ec2b12681e3a823b1d3
Kazuto <mail@kazuto.de>
2026-02-16 16:43:32 +0100

    feat(Components): Export navigation components
    - Add export statement for navigation components
    - Include navigation module in component exports

--- stories/components/index.ts
+++ stories/components/index.ts
@@ -2,6 +2,7 @@ export * from "@/components/core";
 export * from "@/components/display";
 export * from "@/components/feedback";
 export * from "@/components/form";
+export * from "@/components/navigation";
 export { default as Header } from "@/components/layout/Header.vue";
 export { default as Page } from "@/components/layout/Page.vue";
 export * from "@/components/typography";
