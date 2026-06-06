477483ce441f5045aa8217f5dba7e82e313dceb1
Kazuto <mail@kazuto.de>
2026-06-06 19:52:13 +0200

    debug

--- stories/components/core/Link/primitives/Link.vue
+++ stories/components/core/Link/primitives/Link.vue
@@ -1,6 +1,6 @@
 <template>
   <component
-    :is="resolveDynamicComponent(props.tag)"
+    :is="props.tag"
     v-bind="forwardedAttrs"
     :class="linkStyles({ dense, class: classAttr })"
   >
@@ -15,7 +15,7 @@
 </template>
 
 <script lang="ts" setup>
-import { type Component, resolveDynamicComponent } from "vue";
+import { type Component } from "vue";
 import { tv } from "tailwind-variants";
 import { Icon } from "@/components";
 import { useForwardedAttrs } from "@/composables";
