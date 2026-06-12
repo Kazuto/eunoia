7b616c904f067dce42fb11c4f3b330f8e884c046
Kazuto <mail@kazuto.de>
2026-06-12 13:26:25 +0200

    refactor(Layout): Restructure Header and Page components into nested directory
    - Renamed `Header.vue` to `./Header/Header.vue`.
    - Renamed `Header.stories.tsx` to `./Header/Header.stories.tsx`.
    - Updated exports in `index.ts` for Header and Page components.

--- stories/components/layout/index.ts
+++ stories/components/layout/index.ts
@@ -1,4 +1,5 @@
-export { default as Header } from "./Header.vue";
-export { default as Page } from "./Page.vue";
+export { default as Header } from "./Header/Header.vue";
+export { default as Page } from "./Page/Page.vue";
+export type { PageOptions } from "./Page/Page.vue";
 export { default as Sidebar } from "./Sidebar/Sidebar.vue";
 export { default as SidebarToggle } from "./Sidebar/primitives/SidebarToggle.vue";
